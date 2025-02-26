a73b19be0a22d1114f9936c00ef5779613f36f37
Zibo Wei <186524035+Neptunewzb@users.noreply.github.com>
2025-02-26 09:43:51 +0800

    Lec2 ex

---
+++ ex.py
@@ -0,0 +1,11 @@
+from operator import floordiv, mod
+
+def absolute_value(x):
+	"""Rrturn the absolute value of x."""
+	if x < 0
+		return -x
+    elif x==0:
+        return 0
+    else:
+        return x
+
