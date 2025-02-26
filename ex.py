511baa7c00a6c947658cf72d6e844f4d30b838dc
Zibo Wei <186524035+Neptunewzb@users.noreply.github.com>
2025-02-26 16:27:08 +0800

    miscellaneous Python Features

--- ex.py
+++ ex.py
@@ -1,11 +1,25 @@
 from operator import floordiv, mod
 
+def divide_exact(n, d):
+    """Return the quotient and remainder of dividing N by d
+    >>> q, r = divide_exact(2013,10)
+    >>> q
+    201
+    >>> r 
+    3
+    """
+
+    return floordiv(n,d), mod(n, d)
+
 def absolute_value(x):
-	"""Rrturn the absolute value of x."""
-	if x < 0
-		return -x
-    elif x==0:
+    """Return the absolute value of x.
+    
+    """
+    
+    if x < 0:
+        return -x
+    elif x == 0:
         return 0
     else:
         return x
-
+    
