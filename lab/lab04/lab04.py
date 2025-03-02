8ccc7f7c43fdfeaa0c1c68cdefc6bf655b2ff4bb
Zibo Wei <186524035+Neptunewzb@users.noreply.github.com>
2025-03-02 21:09:39 +0800

    lab04 completed

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -7,7 +7,7 @@ def divide(quotients, divisors):
     >>> divide(range(1, 5), range(20, 25))
     {1: [20, 21, 22, 23, 24], 2: [20, 22, 24], 3: [21, 24], 4: [20, 24]}
     """
-    return {____: ____ for ____ in ____}
+    return {q: [d for d in divisors if d%q==0] for q in quotients}
 
 
 def buy(required_fruits, prices, total_amount):
@@ -29,9 +29,9 @@ def buy(required_fruits, prices, total_amount):
             print(cart)
         elif fruits and amount > 0:
             fruit = fruits[0]
-            price = ____
-            for k in ____:
-                add(____, ____, ____)
+            price = prices[fruit]
+            for k in range(1, amount//price+1):
+                add(fruits[1:], amount-price*k, cart+display(fruit, k))
     add(required_fruits, total_amount, '')
 
 
@@ -52,6 +52,28 @@ def display(fruit, count):
 
 
 from math import sqrt
+"""def make_city(name, lat, lon):
+    def select(x):
+        if x == 'name':
+            return name
+        elif x == 'lat':
+            return lat
+        elif x == 'lon':
+            return lon
+        else:
+            return False
+    return select
+
+def get_name(city):
+    return city('name')
+
+def get_lat(city):
+    return city('lat')
+
+def get_lon(city):
+    return city('lon')
+"""
+
 def distance(city_a, city_b):
     """
     >>> city_a = make_city('city_a', 0, 1)
@@ -63,7 +85,7 @@ def distance(city_a, city_b):
     >>> distance(city_c, city_d)
     5.0
     """
-    "*** YOUR CODE HERE ***"
+    return sqrt((get_lat(city_a)-get_lat(city_b))**2 + (get_lon(city_a)-get_lon(city_b))**2)
 
 def closer_city(lat, lon, city_a, city_b):
     """
@@ -80,7 +102,12 @@ def closer_city(lat, lon, city_a, city_b):
     >>> closer_city(41.29, 174.78, bucharest, vienna)
     'Bucharest'
     """
-    "*** YOUR CODE HERE ***"
+    dis1 = (lat - get_lat(city_a))**2 + (lon - get_lon(city_a))**2 
+    dis2 = (lat - get_lat(city_b))**2 + (lon - get_lon(city_b))**2 
+    if dis1 > dis2:
+        return get_name(city_b)
+    else:
+        return get_name(city_a)
 
 def check_city_abstraction():
     """
