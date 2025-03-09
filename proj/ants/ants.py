bc7a505f93ac5ea9a918e96178ba91e376525225
Zibo Wei <186524035+Neptunewzb@users.noreply.github.com>
2025-03-09 10:03:05 +0800

    prob4 finished

--- proj/ants/ants.py
+++ proj/ants/ants.py
@@ -166,6 +166,8 @@ class ThrowerAnt(Ant):
     damage = 1
     # ADD/OVERRIDE CLASS ATTRIBUTES HERE
     food_cost = 3
+    lower_bound = 0
+    upper_bound = float('inf')
 
     def nearest_bee(self):
         """Return the nearest Bee in a Place (that is not the hive) connected to
@@ -174,12 +176,13 @@ class ThrowerAnt(Ant):
         This method returns None if there is no such Bee (or none in range).
         """
         # BEGIN Problem 3 and 4
+        dis = 0
         target = self.place
         while target.is_hive == False:
-            if target.bees != []:
-                return random_bee(target.bees) # REPLACE THIS LINE
-            else:
-                target = target.entrance
+            if dis <= self.upper_bound and dis >= self.lower_bound and target.bees != []:
+                return random_bee(target.bees) 
+            target = target.entrance
+            dis += 1
         return None
         # END Problem 3 and 4
 
@@ -211,8 +214,9 @@ class ShortThrower(ThrowerAnt):
     name = 'Short'
     food_cost = 2
     # OVERRIDE CLASS ATTRIBUTES HERE
+    upper_bound = 3
     # BEGIN Problem 4
-    implemented = False   # Change to True to view in the GUI
+    implemented = True   # Change to True to view in the GUI
     # END Problem 4
 
 
@@ -222,8 +226,9 @@ class LongThrower(ThrowerAnt):
     name = 'Long'
     food_cost = 2
     # OVERRIDE CLASS ATTRIBUTES HERE
+    lower_bound = 5
     # BEGIN Problem 4
-    implemented = False   # Change to True to view in the GUI
+    implemented = True   # Change to True to view in the GUI
     # END Problem 4
 
 
@@ -234,8 +239,10 @@ class FireAnt(Ant):
     damage = 3
     food_cost = 5
     # OVERRIDE CLASS ATTRIBUTES HERE
+    '16:28'
+    health = 3
     # BEGIN Problem 5
-    implemented = False   # Change to True to view in the GUI
+    implemented = True   # Change to True to view in the GUI
     # END Problem 5
 
     def __init__(self, health=3):
