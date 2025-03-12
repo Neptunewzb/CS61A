0584f92347241b1cbdf400293791a27be9889c69
Zibo Wei <186524035+Neptunewzb@users.noreply.github.com>
2025-03-12 10:12:19 +0800

    lab09 finished

--- lab/lab09/lab09.scm
+++ lab/lab09/lab09.scm
@@ -1,10 +1,27 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    (cond((< num1 num2) -1)
+         ((= num1 num2) 0)
+         (else 1)))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (lambda (inc) (+ inc num))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (define (fgx x)
+        (f (g x))
+    )
+    fgx
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (repeat f n)
+    (if (<= n 0)
+        (lambda (x) x)
+        (if (= n 1)
+            f
+            (composed f (repeat f (- n 1))))
+    )
+)
 
 (define (max a b)
   (if (> a b)
@@ -16,4 +33,13 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+    (if (< a b)
+        (gcd b a))
+    (if (= b 1)
+        1)
+    (if (= (modulo a b) 0)
+        b
+        (let ((r (modulo a b)))
+        (gcd b r)))
+)
