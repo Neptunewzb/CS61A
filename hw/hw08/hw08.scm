759cf448ddbf4758660732a7058476d51d217c48
Zibo Wei <186524035+Neptunewzb@users.noreply.github.com>
2025-03-12 18:24:52 +0800

    q3&4 left

--- hw/hw08/hw08.scm
+++ hw/hw08/hw08.scm
@@ -1,6 +1,21 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s) 
+    
+      (if (or (null? s) (null? (cdr s)))
+          #t
+          (let ((first (car s))
+                (rest (cdr s)))
+              (if (> first (car rest))
+                  #f
+                  (ascending? rest)))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
+(define (my-filter pred s)
+  (cond
+    ((null? s) '())  ; Base case: if the list is empty, return empty list
+    ((pred (car s))  ; If the first element satisfies the predicate
+     (cons (car s) (my-filter pred (cdr s))))  ; Include it in the result
+    (else
+     (my-filter pred (cdr s)))  ; Skip the element and continue with the rest
+  ))
 
 (define (interleave lst1 lst2) 'YOUR-CODE-HERE)
 
