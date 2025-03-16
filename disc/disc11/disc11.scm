c1210c6b61a6517e0760548c1878ec4f984894b9
Zibo Wei <186524035+Neptunewzb@users.noreply.github.com>
2025-03-16 23:14:48 +0800

    disc11 finished

--- disc/disc11/disc11.scm
+++ disc/disc11/disc11.scm
@@ -1,7 +1,8 @@
 (define-macro (assign sym1 sym2 expr1 expr2)
   `(begin
-     (define ,sym1 ,expr1)
-     (define ___ ___)))
+     (define temp ,expr2)  ; Evaluate expr2 first and store in a temporary variable
+     (define ,sym1 ,expr1) ; Bind sym1 to expr1
+     (define ,sym2 temp))) ; Bind sym2 to the previously stored value of expr2
 
 (assign x y (+ 1 1) 3)
 (assign x y y x)
@@ -10,10 +11,10 @@
 
 
 (define-macro (switch expr cases)
-    `(let ((val ,expr))
-	  ,(cons
-	    'YOUR-CODE-HERE
-	    (map (lambda (case) (cons
-	           'YOUR-CODE-HERE
-		       (cdr case)))
-		     cases))))
+  `(let ((val ,expr))
+     ,(cons
+       'cond
+       (map (lambda (case) (cons
+              `(equal? val ,(car case))
+              (cdr case)))
+            cases))))
