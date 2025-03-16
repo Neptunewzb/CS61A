4d91d289726b27d29c263c462383e8f28b9386ad
Zibo Wei <186524035+Neptunewzb@users.noreply.github.com>
2025-03-16 14:04:09 +0800

    hw09 q3 left

--- hw/hw09/hw09.scm
+++ hw/hw09/hw09.scm
@@ -1,7 +1,13 @@
-(define (curry-cook formals body) 'YOUR-CODE-HERE)
+(define (curry-cook formals body)
+  (if (null? (cdr formals))  ; If there's only one formal parameter left
+      `(lambda (,(car formals)) ,body)  ; Create a lambda with that parameter and the body
+      `(lambda (,(car formals))  ; Otherwise, create a lambda with the first parameter
+         ,(curry-cook (cdr formals) body))))  ; And recursively curry the rest
 
 (define (curry-consume curry args)
-  'YOUR-CODE-HERE)
+  (if (null? args)
+      curry
+      (curry-consume (curry (car args)) (cdr args))))
 
 (define-macro (switch expr options)
   (switch-to-cond (list 'switch expr options)))
